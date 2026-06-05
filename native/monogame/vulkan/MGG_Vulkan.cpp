d081b0fe2def6536e080482ec0123b5add685d77
roz <andrew@rosolino.com>
2026-06-05 10:41:36 -0400

    Revert "Merge branch 'DX12_fixes' of https://github.com/SickheadGames/MonoGame into DX12_fixes"
    This reverts commit a92abbab81895de5e5750545941c6e8114e3f26f, reversing
    changes made to d8b016d895f5ca2c91f53d03d83a40516f08ede8.

--- native/monogame/vulkan/MGG_Vulkan.cpp
+++ native/monogame/vulkan/MGG_Vulkan.cpp
@@ -468,7 +468,6 @@ struct MGG_GraphicsSystem
 
 static void MGVK_BufferCopyAndFlush(MGG_GraphicsDevice* device, MGG_Buffer* buffer, int destOffset, mgbyte* data, int dataBytes);
 static MGG_Buffer* MGVK_Buffer_Create(MGG_GraphicsDevice* device, MGBufferType type, mgint sizeInBytes, bool no_push);
-static void MGVK_DestroyPipelines(MGG_GraphicsDevice* device, std::function<bool(const MGVK_PipelineState&)> compare);
 static void MGVK_DestroyFrameResources(MGG_GraphicsDevice* device, mgint currentFrame, mgbyte free_all);
 static void MGVK_UpdateRenderPass(MGG_GraphicsDevice* device, FrameCounter currentFrame, VkCommandBuffer commandBuffer);
 static VkCommandBuffer MGVK_BeginNewCommandBuffer(MGG_GraphicsDevice* device);
@@ -1487,12 +1486,6 @@ static void cleanupSwapChain(MGG_GraphicsDevice* device)
 
 		if (usesSwapchain)
 		{
-			// Clean up any pipelines that point at this target set cache.
-			MGVK_DestroyPipelines(device, [targetSetCache](const MGVK_PipelineState& s)
-				{
-					return s.targets == targetSetCache;
-				});
-
 			// This cache entry uses the swapchain, so it's safe to destroy.
 			for (int i = 0; i < MGVK_NUM_TARGETS; ++i)
 			{
